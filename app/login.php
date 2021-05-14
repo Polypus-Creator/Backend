9563051305a57742f8418bd314c1b746022830f8
Eudald Cardona <me@eudald.me>
2021-05-14 17:52:25 +0300

    Login endpoint (first pass)

---
+++ app/login.php
@@ -0,0 +1,33 @@
+<?php
+require_once('private/database.php');
+include_once('private/endpoints.php');
+
+global $database;
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if ($username == null) die(json_encode(array("error" => "Please enter a username")));
+if ($password == null) die(json_encode(array("error" => "Please enter a password")));
+
+try {
+    pg_prepare($database, "query_user_$username",
+        'select id, username, password, create_date, token from users where username = $1');
+    $result = pg_execute($database, "query_user_$username", array($username));
+    if (pg_num_rows($result) == 0) die(json_encode(array("error" => "Incorrect credentials")));
+    $user = pg_fetch_row($result);
+    if (password_verify($password, $user[2])) {
+        echo json_encode(array(
+            "error" => false,
+            "body" => array(
+                "id" => $user[0],
+                "username" => $user[1],
+                "create_date" => $user[3],
+                "token" => $user[4])));
+    } else {
+        die(json_encode(array('error' => 'Incorrect credentials')));
+    }
+} catch (Exception $e) {
+
+    http_response_code(500);
+    die(json_encode(array('error' => 'Unknown error occurred. Please try again')));
+}
