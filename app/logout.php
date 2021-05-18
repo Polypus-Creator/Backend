53f753627e9923357a66d048033c9494ed9a09ad
Eudald Cardona <me@eudald.me>
2021-05-18 15:48:23 +0300

    Logout endpoint

---
+++ app/logout.php
@@ -0,0 +1,16 @@
+<?php
+require_once('private/secure_endpoint.php');
+
+global $database;
+global $token;
+
+pg_prepare($database, "logout_user_$token",
+    "update users set token = null where token = $1");
+$result = pg_execute($database, "logout_user_$token", array($token));
+
+if (pg_affected_rows($result) == 0) {
+    http_response_code(500);
+    die(json_encode(array("error" => "Unexpected error, please try again")));
+} else {
+    echo json_encode(array("error" => false, "body" => "Logged out"));
+}
