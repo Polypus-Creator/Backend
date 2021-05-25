3bc4582b1998458eaca1e99253208787d446854b
Eudald Cardona <me@eudald.me>
2021-05-25 16:19:51 +0200

    CTA template parsing

--- app/private/util/body_parser.php
+++ app/private/util/body_parser.php
@@ -6,6 +6,10 @@ function parse_body($element): string
     switch ($name) {
         case "Separator":
             return parse_separator();
+        case "CallToActionMiddle":
+            return parse_cta_middle($element);
+        case "CallToActionLeft":
+            return parse_cta_left($element);
     }
     return "";
 }
@@ -13,4 +17,17 @@ function parse_body($element): string
 function parse_separator(): string
 {
     return file_get_contents(body_dir . "/Divider.html");
+}
+
+function parse_cta_middle($element): string
+{
+    return str_replace(["%title", "%content", "%button1text", "%button2text", "#1", "#2"],
+        [$element["Title"], $element["Text"], $element["ButtonText"], "TODO", $element["ButtonUrl"], "#"],
+        file_get_contents(body_dir . "/CallToActionMiddle.html"));
+}
+function parse_cta_left($element): string
+{
+    return str_replace(["%title", "%content", "%buttonText", "#1"],
+        [$element["Title"], $element["Text"], $element["ButtonText"], $element["ButtonUrl"]],
+        file_get_contents(body_dir . "/CallToActionLeft.html"));
 }
