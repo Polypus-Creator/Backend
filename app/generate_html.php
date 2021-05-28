483acdb2acaa8a7fd16504f77e3ef45bee16004c
Eudald Cardona <me@eudald.me>
2021-05-28 17:54:07 +0200

    Refactor element parser into an object to store website details

--- app/generate_html.php
+++ app/generate_html.php
@@ -8,14 +8,25 @@ const body_dir = "private/blocks/body/";
 const header_dir = "private/blocks/header/";
 const footer_dir = "private/blocks/footer/";
 
+global $database;
+global $user;
+
+$stmt_name = "query_website_details_${$user["id"]}";
+pg_prepare($database, $stmt_name,
+    'select * from webs where user_id = $1');
+$result = pg_execute($database, $stmt_name, array($user["id"]));
+if (pg_num_rows($result) == 0) die(json_encode(array("error" => "Website not found")));
+$details = pg_fetch_assoc($result);
+
 $web_name = $_GET["WebName"];
 $elements = $_GET["ListaJSON"];
 //add first element of HTML
 $html = parse_top_shell($web_name);
+$parser = new ElementParser($details);
 //parse each element
 foreach ($elements as $element) {
     $element_name = $element["Element"];
-    $html = $html . parse_element($element);
+    $html = $html . $parser->parse_element($element);
 }
 //add the body and html closing tags
 $html = $html . file_get_contents(structure_dir . "BottomStructure.html");
--- app/private/util/element_parser.php
+++ app/private/util/element_parser.php
@@ -1,53 +1,64 @@
 <?php
 require_once('header_parser.php');
 
-function parse_element($element): string
+class ElementParser
 {
-    $name = $element["Elemento"];
-    switch ($name) {
-        case "Logo&ItemsNav":
-            return parse_header_items($element);
-        case "Separator":
-            return parse_separator();
-        case "CallToActionMiddle": //todo check new name
-            return parse_cta($element);
-        case "Image Text":
-            return parse_image_text($element);
-        case "Footer": //todo check name
-            return parse_footer($element);
+    private $details;
+
+    public function __construct(array $details)
+    {
+        $this->details = $details;
     }
-    return "";
-}
 
-function parse_separator(): string
-{
-    return file_get_contents(body_dir . "Divider.html");
-}
 
-function parse_cta($element): string
-{
-    $file_types = array("Left" => "CallToActionLeft.html", "Middle" => "CallToActionMiddle.html",
-        "Right" => "CallToActionRight.html");
-    if (isset($file_types[$element["Orientation"]])) {
-        $file = $file_types[$element["Orientation"]];
-    } else die(array("error" => "Unrecognised value for orientation '${$element["Orientation"]}'"));
+    function parse_element($element): string
+    {
+        $name = $element["Elemento"];
+        switch ($name) {
+            case "Logo&ItemsNav":
+                return parse_header_items($element, $this->details);
+            case "Separator":
+                return $this->parse_separator();
+            case "CallToActionMiddle": //todo check new name
+                return $this->parse_cta($element);
+            case "Image Text":
+                return $this->parse_image_text($element);
+            case "Footer": //todo check name
+                return $this->parse_footer($element);
+        }
+        return "";
+    }
 
-    return str_replace(["%title", "%content", "%buttonText", "%buttonLink"],
-        [$element["Title"] ?? "", $element["Text"] ?? "", $element["ButtonText"] ?? "", $element["ButtonUrl"] ?? "#"],
-        file_get_contents(body_dir . $file));
-}
+    private function parse_separator(): string
+    {
+        return file_get_contents(body_dir . "Divider.html");
+    }
 
-function parse_image_text($element): string
-{
-    $file = $element["Orientaton"] === "Left" ? "ImageAndTextLeft.html" : "ImageAndTextRight.html";
-    return str_replace(["%title", "%content", "%imageUrl"],
-        [$element["Title"], $element["Text"], $element["Image"]],
-        file_get_contents(body_dir . $file));
-}
+    private function parse_cta($element): string
+    {
+        $file_types = array("Left" => "CallToActionLeft.html", "Middle" => "CallToActionMiddle.html",
+            "Right" => "CallToActionRight.html");
+        if (isset($file_types[$element["Orientation"]])) {
+            $file = $file_types[$element["Orientation"]];
+        } else die(array("error" => "Unrecognised value for orientation '${$element["Orientation"]}'"));
 
-function parse_footer($element): string
-{
-    return str_replace("%footerText",
-        $element["Text"],
-        file_get_contents(footer_dir . "SimpleFooter.html"));
+        return str_replace(["%title", "%content", "%buttonText", "%buttonLink"],
+            [$element["Title"] ?? "", $element["Text"] ?? "", $element["ButtonText"] ?? "", $element["ButtonUrl"] ?? "#"],
+            file_get_contents(body_dir . $file));
+    }
+
+    private function parse_image_text($element): string
+    {
+        $file = $element["Orientaton"] === "Left" ? "ImageAndTextLeft.html" : "ImageAndTextRight.html";
+        return str_replace(["%title", "%content", "%imageUrl"],
+            [$element["Title"], $element["Text"], $element["Image"]],
+            file_get_contents(body_dir . $file));
+    }
+
+    private function parse_footer($element): string
+    {
+        return str_replace("%footerText",
+            $element["Text"],
+            file_get_contents(footer_dir . "SimpleFooter.html"));
+    }
 }
