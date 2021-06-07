6dc936f055b317fd323b5bb0b6e1ae7f86db2d6a
Eudald Cardona <me@eudald.me>
2021-06-07 16:03:18 +0200

    Fix names of update_design endpoint

--- app/update_design.php
+++ app/update_design.php
@@ -8,12 +8,12 @@ $stmt_name = "update_design_${$user["id"]}";
 $stmt = pg_prepare($database, $stmt_name, 'update webs set website_name = $1, description = $2, category = $3, '
     . 'primary_colour = $4, secondary_colour = $5, font = $6 where user_id = $7');
 $result = pg_execute($database, $stmt_name, [
-    $_GET["website_name"],
-    $_GET["description"],
-    $_GET["category"],
-    $_GET["primary_colour"],
-    $_GET["secondary_colour"],
-    $_GET["font"],
+    $_GET["Website_name"],
+    $_GET["Description"],
+    $_GET["Category"],
+    $_GET["Primary_colour"],
+    $_GET["Secondary_colour"],
+    $_GET["Font"],
     $user["id"]
 ]);
 
