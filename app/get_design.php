bf7aa67219fa4ce1e0356506bfd7c079e6089c2a
Eudald Cardona <me@eudald.me>
2021-06-10 18:26:11 +0200

    Consistent get and update design JSON

--- app/get_design.php
+++ app/get_design.php
@@ -9,4 +9,11 @@ $stmt = pg_prepare($database, $stmt_name, 'select * from webs where user_id = $1
 $result = pg_execute($database, $stmt_name, [$user["id"]]);
 $details = pg_fetch_assoc($result);
 unset($details["data"]);
-echo json_encode(["error" => false, "body" => $details]);
+echo json_encode(["error" => false, "body" => [
+    "Website_name" => $details["website_name"],
+    "Description" => $details["description"],
+    "Category" => $details["category"],
+    "Primary_colour" => $details["primary_colour"],
+    "Secondary_colour" => $details["secondary_colour"],
+    "Font" => $details["font"],
+]]);
